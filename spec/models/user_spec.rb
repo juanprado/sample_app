3bc5ef8f3aded8549ec335826c41653c6e273a32
Juan Prado <juanp@izea.com>
2013-01-13 00:00:00 -0500

    alt syntax for user before save

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -33,15 +33,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "when name is too long" do
+    before { @user.name = "a" * 51 }
+    it { should_not be_valid }
+  end
+
   describe "when email is not present" do
     before { @user.email = " " }
     it { should_not be_valid }
   end
 
-  describe "when name is too long" do
-    before { @user.name = "a" * 51 }
-    it { should_not be_valid }
-  end
 
   describe "when email format is invalid" do
     it "should be invalid" do
@@ -50,6 +51,17 @@ describe User do
         @user.email = invalid_address
         @user.should_not be_valid
       end      
+    end
+  end
+
+  
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
     end
   end
 
